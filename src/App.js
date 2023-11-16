b2c0df0d893d945f5fc2a72fb34b9c41592b21fd
Amisha <shahamisha012@gmail.com>
2023-11-16 23:45:56 +0530

    create buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+import { useState } from "react";
+
 const App = () => {
+
+  const [tasks,setTasks] = useState([]);
+  const [task, setTask] = useState("");
+
+  const addTasks = () => {
+    if(task!==""){
+      setTasks([...tasks,task])
+      setTask("");
+      console.log(tasks);
+   }
+  }
+
   return(
-    <h1 className="bg-slate-500">
-      This is working again
-    </h1>
-    
-  )
+   <div className="flex flex-col items-center">
+    <h1 className="text-4xl m-16 font-bold">Simple TODO App</h1> 
+    <div className="p-6">
+      <input className="bg-slate-100 rounded-md p-4 m-4"
+      type="text"
+      value={task}
+      onClick={(e)=>{
+        setTask(e.target.value);
+      }}
+      placeholder="create a new todo"
+      />
+      <button onChange={addTasks}
+      className="bg-green-500 text-white p-3 m-3 rounded-md font-bold hover:bg-green-600" >Add Tasks</button>
+    </div>
+    <div>
+      
+    </div>
+    </div>
+  );
 }
 
 export default App
